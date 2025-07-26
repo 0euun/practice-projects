9c6876f09c13c55c69f69f4fcb26d405a5ec9176
0euun <jeyejong70@naver.com>
2025-07-27 01:02:04 +0900

    CustomUser 모델 생성

--- Ppractice/users/models.py
+++ Ppractice/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=False)
+    kakao_oid = models.BigIntegerField(null=True, unique=True, blank=False)
+    profile_image = models.URLField(max_length=200, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
